5a58b5b07187318e1a8e467510bbf98409994aef
ashishsurya <ashishsurya126@gmail.com>
2022-09-30 20:51:48 +0530

    error handling

--- pages/api/get-pinned-notes.js
+++ pages/api/get-pinned-notes.js
@@ -2,16 +2,15 @@
 import Note from '../../models/Note';
 import connectMongoose from '../../utils/connectMongo';
 
-
-
-export default async function getPinnedNotes(
-  req,
-  res
-) {
+export default async function getPinnedNotes(req, res) {
   console.log('CONNECTING TO MONGO');
   await connectMongoose();
   console.log('CONNECTED TO MONGO');
 
-  const notes = await Note.find({pinned:true}).then((data) => data);
-  return res.json({ notes });
+  try {
+    const notes = await Note.find({ pinned: true }).then((data) => data);
+    return res.json({ notes });
+  } catch (error) {
+    return res.json({ notes: [], error: error.message });
+  }
 }
