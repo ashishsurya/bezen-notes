902d69dca18ec984e1c0844881671ee152f08d98
ashishsurya <ashishsurya126@gmail.com>
2022-10-01 14:59:45 +0530

    cleared errors

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 const connectMongoose = async () =>
   mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URI, {
     //must add in order to not get any error masseges:
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
   });
 
 export default connectMongoose;
