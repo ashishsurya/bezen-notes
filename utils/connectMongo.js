8c1511fdf6b8dd99ec5116bef6978626309f133c
ashishsurya <ashishsurya126@gmail.com>
2022-10-01 14:57:55 +0530

    djuv

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,7 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-
-const connectMongoose = async () => mongoose.connect(process.env.MONGO_DB_URI)
-
+const connectMongoose = async () =>
+  mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URI, {
+    //must add in order to not get any error masseges:
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
 
 export default connectMongoose;
